cc3be5eb088d8d7dec9534551d3ca3202cbe7bb3
yungsavkas <savkas30@gmail.com>
2023-02-02 21:52:40 +0300

    coding :)

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import express from 'express'
 import mysql from "mysql";
 import cors from 'cors'
-import * as path from "path";
 
-const PORT = process.env.PORT || 8800;
+import https from 'https'
+import fs from 'fs'
+
+const options = {
+    key: fs.readFileSync('certificate/private.pem'),
+    cert: fs.readFileSync('certificate/cert.pem')
+}
+
+const PORT = process.env.PORT || 8080;
+
 const app = express();
-// app.use(express.static(__dirname));
-// app.use(express.static(path.resolve(__dirname,'build')))
 app.use(express.json());
 app.use(cors())
 
 
 const db = mysql.createConnection({
-    host: "localhost",
+    host: "194.67.112.147",
+    port: '/var/run/mysqld/mysqld.sock',
     user: "root",
-    password: "root",
+    password: "sava606348",
     database: "socksshop"
 
 })
@@ -27,45 +34,39 @@ db.connect((err) =>{
     }
 });
 
-// app.get("*",(req,res)=>{
-//     res.sendFile(path.join(__dirname,'build','index.html'))
-// })
 
 app.get("/", (req,res)=> {
     res.json("Welcome on SocksShopBy!");
 })
 
-
 app.get("/products", (req,res)=> {
     const q = "SELECT * FROM goods"
-    db.query(q, (err,data)=> {
-        if (err) return res.json(err);
-        else return res.json(data);
-    })
+        db.query(q, (err,data)=> {
+            if (err) return res.json(err);
+            else return res.json(data);
+        })
 })
-
-
 app.post("/addGoods", (req,res)=> {
     const q = "INSERT INTO goods(`info`)  VALUES (?)"
     const values = [
         ...req.body
     ];
-
-    db.query(q,[values], (err,data)=> {
-        if (err) return res.json(err);
-        else return res.json(data);
-    })
+        db.query(q,[values], (err,data)=> {
+            if (err) return res.json(err);
+            else return res.json(data);
+        })
 })
-
 app.post("/deleteItem", (req,res)=> {
     const q = `DELETE FROM goods WHERE id = ${req.body[0]}`
-    db.query(q, (err,data)=> {
-        if (err) return res.json(err);
-        else return res.json(data);
-    })
+        db.query(q, (err,data)=> {
+            if (err) return res.json(err);
+            else return res.json(data);
+        })
 })
 
-
-app.listen(PORT, ()=>{
-    console.log('Сервер подключен успешно')
+https.createServer(options, (req,res)=>{
+    console.log('SSL ADDED')
+    console.log("Url: " + req.url);
+}).listen(PORT, ()=>{
+    console.log(`Сервер подключен успешно port=${PORT}`)
 })
